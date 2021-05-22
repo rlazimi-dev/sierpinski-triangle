66917babfe5fb15af7b1f4bf002a78f8e2e75e36
Roni Lazimi <ron@RonMAC.local>
2021-05-21 22:22:14 -0400

    serpienski triangle

--- serpienski-triangle.py
+++ serpienski-triangle.py
@@ -2,18 +2,19 @@ import turtle
 import math
 import time
 
-canvas = turtle.Turtle()
-
-screen_width = canvas.screen.window_width()
-screen_height = canvas.screen.window_height()
-canvas.screen.setworldcoordinates(0, 0, screen_width, screen_height)
-
 """
 TODO:
 - define the functions needed to make the recursive implementation possible
 - triangles are equilateral - 60deg turns
 - define the function
 """
+
+canvas = turtle.Turtle()
+
+screen_width = canvas.screen.window_width()
+screen_height = canvas.screen.window_height()
+canvas.screen.setworldcoordinates(0, 0, screen_width, screen_height)
+
 
 class Rect:
   def __init__(self,top: int, right: int, bottom: int, left: int):
@@ -26,6 +27,12 @@ class Rect:
     self.right = right
     self.bottom = bottom
     self.left = left
+
+  def height(self):
+    return self.top - self.bottom
+
+  def width(self):
+    return self.right - self.left
 
 
 def draw_rect(canvas, r):
@@ -40,8 +47,8 @@ def draw_rect(canvas, r):
 
 def inscribe_triangle(canvas, r):
 
-    height = r.top - r.bottom
-    leg = (r.right - r.left) / 2
+    height = r.height()
+    leg = r.width() / 2
 
     canvas.penup()
     #go to bottom left of triangle (avoids calculating top angle twice)
@@ -71,6 +78,67 @@ def inscribe_triangle(canvas, r):
     canvas.right(180 - theta)
     canvas.forward(r.right - r.left)
 
+    #reset direction
+    canvas.right(180)
 
+
+#when calling on rect, partition the rect into three subrects and assume that serp on those rects is displayed
+#what's left is to return
+'''
+breaking up r into 3 Rects:
+.__________.
+|   |   |  |
+|___|___|__|
+|     |    |
+|_____|____|
+'''
+
+def serp(canvas, r, base_width=100, draw_subproblems=False, draw_bounds=True):
+  if r.height() < base_width:
+    inscribe_triangle(canvas,r)
+  else:
+    rt = Rect(
+      r.top,
+      r.right - r.width() / 4,
+      r.top - r.height() / 2,
+      r.left + r.width() / 4
+    )
+
+    rl = Rect(
+      r.top - r.height() / 2,
+      r.right - r.width() / 2,
+      r.bottom,
+      r.left
+    )
+
+    rr = Rect(
+      r.top - r.height() / 2,
+      r.right,
+      r.bottom,
+      r.left + r.width() / 2
+    )
+
+    #not necessary but looks better
+    if draw_subproblems:
+      draw_rect(canvas, r)
+    if draw_bounds:
+      inscribe_triangle(canvas, r)
+
+    #necessary
+    serp(canvas, rt, base_width, draw_subproblems, draw_bounds)
+    serp(canvas, rl, base_width, draw_subproblems, draw_bounds)
+    serp(canvas, rr, base_width, draw_subproblems, draw_bounds)
+
+
+serp(
+  canvas,
+  Rect(
+    screen_height * 0.9,
+    screen_width * 0.9,
+    screen_height * 0.1,
+    screen_width * 0.1
+  ),
+  50
+)
 
 time.sleep(2)
